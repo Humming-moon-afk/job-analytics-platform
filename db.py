0d710614dc29c4be5450989688fa8643e7de02ab
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-11 12:56:17 +0200

    feat(backend): implement databse configuration with sqlite3

--- db.py
+++ db.py
@@ -1,4 +1,17 @@
 import sqlite3
 
 def init_db():
-    sqlite3.connect('jobs.db')
+    connection = sqlite3.connect('jobs.db')
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS job_urls(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    url TEXT UNIQUE,
+    source TEXT,
+    status TEXT DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+    connection.commit()
+    connection.close()
+
+init_db()
